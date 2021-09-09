42de0a949342d7acaf468c4caa11ec6658f108fd
Diego Alejandro Ochoa Gutierrez <diego.ochoa@globant.com>
2021-09-09 13:49:35 -0500

    Add spring boot boilerplate

---
+++ api/src/test/java/com/mercadolibre/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
