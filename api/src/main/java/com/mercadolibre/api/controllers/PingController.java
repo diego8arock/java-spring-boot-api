2eca50dd8fcbba5952eedcfc9e2bfe0ef2a71fb5
Diego Alejandro Ochoa Gutierrez <diego.ochoa@globant.com>
2021-09-10 09:32:41 -0500

    Add ping controller

---
+++ api/src/main/java/com/mercadolibre/api/controllers/PingController.java
@@ -0,0 +1,18 @@
+package com.mercadolibre.api.controllers;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+    
+    @GetMapping("/ping")
+    String ping(){
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        return dtf.format(now);
+    }
+
+}
