4d45d0972e5b5997910b4e8772463243029a8106
Diego Alejandro Ochoa Gutierrez <diego.ochoa@globant.com>
2021-09-15 13:58:12 -0500

    Add missing methods

--- api/src/main/java/com/mercadolibre/api/dtos/UserItemDTOCompositeKey.java
+++ api/src/main/java/com/mercadolibre/api/dtos/UserItemDTOCompositeKey.java
@@ -19,4 +19,31 @@ public class UserItemDTOCompositeKey  implements Serializable{
     public void setUser(Long user) {
         this.user = user;
     }
+
+    @Override
+    public int hashCode(){
+        return this.item.hashCode() * this.user.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+ 
+        // If the object is compared with itself then return true 
+        if (o == this) {
+            return true;
+        }
+ 
+        /* Check if o is an instance of Complex or not
+          "null instanceof [type]" also returns false */
+        if (!(o instanceof UserItemDTOCompositeKey)) {
+            return false;
+        }
+         
+        // typecast o to Complex so that we can compare data members
+        UserItemDTOCompositeKey c = (UserItemDTOCompositeKey) o;
+         
+        // Compare the data members and return accordingly
+        return this.item.compareToIgnoreCase(c.getItem()) == 0
+                && Long.compare(this.user, c.getUser()) == 0;
+    }
 }
